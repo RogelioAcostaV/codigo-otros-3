800121442fa7f4c8c1b00d0a4a82814d32fd8b3c
Rogelio AV <acostavilledar@gmail.com>
2025-01-24 14:57:11 -0600

    se modifican los archivos

--- filtro.js
+++ filtro.js
@@ -8,8 +8,25 @@ const productos = [
   {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
 ]
 
-const li = document.getElementsByName("lista-de-productos")
-const $i = document.querySelector('.input');
+const li = document.getElementById("lista-de-productos")
+//Cambie get.ElementsById por getElementById
+const $i = document.querySelector('input');
+//Le quite el punto input para pudiera acceder a los elementos en el HTML
+function displayProductos(productos) {
+  for (let i = 0; i < productos.length; i++) {
+    var d = document.createElement("div")
+    d.classList.add("producto")
+    var ti = document.createElement("p")
+    ti.classList.add("titulo")
+    ti.textContent = productos[i].nombre
+    var imagen = document.createElement("img");
+    imagen.setAttribute('src', productos[i].img);
+    d.appendChild(ti)
+    d.appendChild(imagen)
+    li.appendChild(d)
+  }
+}
+//Agregue la funcion que se manda a llamar para mostrar los productos porque no existia 
 
 for (let i = 0; i < productos.length; i++) {
   var d = document.createElement("div")
